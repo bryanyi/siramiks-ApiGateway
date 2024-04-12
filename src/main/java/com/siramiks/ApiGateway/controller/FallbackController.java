ec81d1650f0f1a4793dd85233201e14626bd1a81
bryanyi <bryanyi1994@gmail.com>
2024-04-11 22:23:48 -0700

    API Gateway fully working. Will work on circuit breakers later. I want to focus more on testing and deployment strategies

--- src/main/java/com/siramiks/ApiGateway/controller/FallbackController.java
+++ src/main/java/com/siramiks/ApiGateway/controller/FallbackController.java
@@ -2,25 +2,30 @@ package com.siramiks.ApiGateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/fallback")
+@RequestMapping(
+        value = "/fallback",
+        produces = "application/json",
+        method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+)
 public class FallbackController {
 
-  @GetMapping("productServiceFallback")
+  @GetMapping("/productServiceFallback")
+  @PostMapping("/productServiceFallback")
   public ResponseEntity<String> productServiceFallback() {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Issue detected in product-service");
   }
 
-  @GetMapping("orderServiceFallback")
+  @GetMapping("/orderServiceFallback")
+  @PostMapping("/orderServiceFallback")
   public ResponseEntity<String> orderServiceFallback() {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Issue detected in order-service");
   }
 
-  @GetMapping("paymentServiceFallback")
+  @GetMapping("/paymentServiceFallback")
+  @PostMapping("/paymentServiceFallback")
   public ResponseEntity<String> paymentServiceFallback() {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Issue detected in payment-service");
   }
